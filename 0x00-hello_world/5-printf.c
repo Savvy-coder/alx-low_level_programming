70f8f6820d0a1cf17bb92500ac33b7849d291c7d
Savvy-coder <hashimiyyu19@gmail.com>
2022-03-10 13:45:59 -0800

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - function needs to return an integer after execution
+*Return: return 0 as success and 1 as failure
+*/
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
