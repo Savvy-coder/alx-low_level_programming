1572837c2910c298dbf9a38c2c1f708e6b49eec2
Savvy-coder <hashimiyyu19@gmail.com>
2022-03-10 14:53:51 -0800

    print quote to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 
 /**
 *main - entry point
-*Return: return 0 as success
-**/
+*Return - return 0 as success
+*/
 
-int main(void)
+int main(viod)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
