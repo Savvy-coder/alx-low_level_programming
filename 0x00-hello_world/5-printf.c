2f31ad6b11666add32a48dc710cb4dd629ded180
Savvy-coder <hashimiyyu19@gmail.com>
2022-03-10 15:09:29 -0800

    5 print commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
 *main - function needs to return an integer after execution
-*Return: return 0 as success and 1 as failure
+*Return: return 0 as success and 1 as fail
 */
 
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
