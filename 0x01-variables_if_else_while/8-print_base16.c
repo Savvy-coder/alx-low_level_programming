dc3b0999c1792dd04d35b775ca1628a4f6c21079
Savvy-coder <hashimiyyu19@gmail.com>
2022-03-11 16:36:23 -0800

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,14 +4,14 @@
  * main - Entry point
  * Description: prints all numbers of base 16 in lowercase
  * Return: return 0 as success
- * */
+ */
 
 int main(void)
 {
 	int num = 0;
 	char letter = 'a';
 
-	while ( num < 10);
+	while (num < 10);
 	{
 		putchar(num + '0');
 		num++;
