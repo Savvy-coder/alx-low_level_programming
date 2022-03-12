c55bfa4fe156a5f27eadc93c77bfa0ad047265d7
Savvy-coder <hashimiyyu19@gmail.com>
2022-03-12 15:29:28 -0800

    last commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,29 +8,36 @@
 
 int main(void)
 {
-	int a, b, c, d;
+	int i, j, k;
 
-	for (a = 0; a < 1000; a++)
+	i = 48;
+	j = 48;
+	k = 48;
+
+	while (i < 58)
 	{
-		b = a / 100;
-		c = (a / 10) % 10;
-		d = a % 10;
-
-		if (b < c && c < d)
+		j = i + 1;
+		while (j < 58)
 		{
-			putchar(b + '0');
-			putchar(c + '0');
-			putchar(d + '0');
-
-			if (a < 700)
-			{
-				putchar(44);
-				putchar(32);
-			}
+			k = j + 1;
+				while (k < 58)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i < 55 || j < 56 || k < 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+					k++;
+				}
+				j++;
 		}
+		i++;
 	}
 
-	putchar('\n');
+	putchar(10);
 
 	return (0);
 }
